5a069ca8f7b2f8b96615ae90964d628b610d5b08
Carlos Rocha <carlos.rvrocha@gmail.com>
2018-02-04 15:59:07 +1100

    Cleaned up and allows to mask land/sea

--- plot_precip_clim.py
+++ plot_precip_clim.py
@@ -44,6 +44,17 @@ def plot_data(cube, month, gridlines=False):
     title = '%s precipitation climatology (%s)' %(cube.attributes['model_id'], month)
     plt.title(title)
 
+def apply_mask(pr_cube, sftlf_cube, realm):
+    """Mask ocean using a sftlf (land surface fraction) file."""
+   
+    if realm == 'land':
+        mask = numpy.where(sftlf_cube.data > 50, True, False)
+    else:
+        mask = numpy.where(sftlf_cube.data < 50, True, False)
+   
+    pr_cube.data = numpy.ma.asarray(pr_cube.data)
+    pr_cube.data.mask = mask
+
 
 def main(inargs):
     """Run the program."""
@@ -51,6 +62,12 @@ def main(inargs):
     cube = read_data(inargs.infile, inargs.month)    
     cube = convert_pr_units(cube)
     clim = cube.collapsed('time', iris.analysis.MEAN)
+    
+    if inargs.mask:
+        sftlf_file, realm = inargs.mask
+        sftlf_cube = iris.load_cube(sftlf_file, 'land_area_fraction')
+        clim = apply_mask(clim, sftlf_cube, realm
+    
     plot_data(clim, inargs.month)
     plt.savefig(inargs.outfile)
 
@@ -63,6 +80,10 @@ if __name__ == '__main__':
     parser.add_argument("month", type=str, choices=calendar.month_abbr[1:], 
                     help="Month to plot")
     parser.add_argument("outfile", type=str, help="Output file name")
+
+    parser.add_argument("--mask", type=str, nargs=2, metavar=('SFTLF_FILE', 'REALM'), default=None,
+                           help='Apply a land or ocean mask (specify the realm to mask)')
+
 
     args = parser.parse_args()
     
